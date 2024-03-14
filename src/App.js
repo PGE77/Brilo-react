1d706fd6dc79aa3fc4076054234692515df60d4f
Petr Gargulák <gargulakpetr@seznam.cz>
2024-03-14 14:03:05 +0100

    prefixy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./dist/css/App.css";
-import "./Navbar/Navbar.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 
